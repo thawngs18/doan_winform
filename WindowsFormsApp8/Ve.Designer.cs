d218120584b3ea03272563669d7ca1cee0102917
Tài Nguyễn Tấn <nguyentantai220804@gmail.com>
2024-12-18 22:16:47 +0700

    indclogordo

--- WindowsFormsApp8/Ve.Designer.cs
+++ WindowsFormsApp8/Ve.Designer.cs
@@ -35,27 +35,27 @@
             this.lblTenPhim = new System.Windows.Forms.Label();
             this.lblPhong = new System.Windows.Forms.Label();
             this.toolStrip1 = new System.Windows.Forms.ToolStrip();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.pnlQR = new System.Windows.Forms.Panel();
             this.toolStripButton1 = new System.Windows.Forms.ToolStripButton();
             this.toolbtnPrint = new System.Windows.Forms.ToolStripButton();
             this.pnlVe.SuspendLayout();
             this.toolStrip1.SuspendLayout();
+            this.groupBox1.SuspendLayout();
             this.SuspendLayout();
             // 
             // pnlVe
             // 
-            this.pnlVe.Controls.Add(this.lblTenghe);
-            this.pnlVe.Controls.Add(this.lblDate);
-            this.pnlVe.Controls.Add(this.lblTenPhim);
-            this.pnlVe.Controls.Add(this.lblPhong);
-            this.pnlVe.Location = new System.Drawing.Point(67, 44);
+            this.pnlVe.Controls.Add(this.groupBox1);
+            this.pnlVe.Location = new System.Drawing.Point(3, 28);
             this.pnlVe.Name = "pnlVe";
-            this.pnlVe.Size = new System.Drawing.Size(287, 261);
+            this.pnlVe.Size = new System.Drawing.Size(364, 354);
             this.pnlVe.TabIndex = 1;
             // 
             // lblTenghe
             // 
             this.lblTenghe.AutoSize = true;
-            this.lblTenghe.Location = new System.Drawing.Point(32, 167);
+            this.lblTenghe.Location = new System.Drawing.Point(14, 180);
             this.lblTenghe.Name = "lblTenghe";
             this.lblTenghe.Size = new System.Drawing.Size(35, 13);
             this.lblTenghe.TabIndex = 3;
@@ -64,7 +64,7 @@
             // lblDate
             // 
             this.lblDate.AutoSize = true;
-            this.lblDate.Location = new System.Drawing.Point(32, 121);
+            this.lblDate.Location = new System.Drawing.Point(14, 134);
             this.lblDate.Name = "lblDate";
             this.lblDate.Size = new System.Drawing.Size(35, 13);
             this.lblDate.TabIndex = 2;
@@ -73,7 +73,7 @@
             // lblTenPhim
             // 
             this.lblTenPhim.AutoSize = true;
-            this.lblTenPhim.Location = new System.Drawing.Point(32, 72);
+            this.lblTenPhim.Location = new System.Drawing.Point(14, 85);
             this.lblTenPhim.Name = "lblTenPhim";
             this.lblTenPhim.Size = new System.Drawing.Size(35, 13);
             this.lblTenPhim.TabIndex = 1;
@@ -82,7 +82,7 @@
             // lblPhong
             // 
             this.lblPhong.AutoSize = true;
-            this.lblPhong.Location = new System.Drawing.Point(32, 30);
+            this.lblPhong.Location = new System.Drawing.Point(14, 43);
             this.lblPhong.Name = "lblPhong";
             this.lblPhong.Size = new System.Drawing.Size(35, 13);
             this.lblPhong.TabIndex = 0;
@@ -95,9 +95,30 @@
             this.toolbtnPrint});
             this.toolStrip1.Location = new System.Drawing.Point(0, 0);
             this.toolStrip1.Name = "toolStrip1";
-            this.toolStrip1.Size = new System.Drawing.Size(452, 25);
+            this.toolStrip1.Size = new System.Drawing.Size(367, 25);
             this.toolStrip1.TabIndex = 2;
             this.toolStrip1.Text = "toolStrip1";
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.lblPhong);
+            this.groupBox1.Controls.Add(this.lblTenghe);
+            this.groupBox1.Controls.Add(this.lblTenPhim);
+            this.groupBox1.Controls.Add(this.lblDate);
+            this.groupBox1.Location = new System.Drawing.Point(3, 3);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(361, 357);
+            this.groupBox1.TabIndex = 4;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Thông Tin Vé";
+            // 
+            // pnlQR
+            // 
+            this.pnlQR.Location = new System.Drawing.Point(3, 27);
+            this.pnlQR.Name = "pnlQR";
+            this.pnlQR.RightToLeft = System.Windows.Forms.RightToLeft.No;
+            this.pnlQR.Size = new System.Drawing.Size(364, 364);
+            this.pnlQR.TabIndex = 4;
             // 
             // toolStripButton1
             // 
@@ -123,7 +144,8 @@
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(452, 383);
+            this.ClientSize = new System.Drawing.Size(367, 383);
+            this.Controls.Add(this.pnlQR);
             this.Controls.Add(this.toolStrip1);
             this.Controls.Add(this.pnlVe);
             this.IsMdiContainer = true;
@@ -131,9 +153,10 @@
             this.Text = "Ve";
             this.Load += new System.EventHandler(this.Ve_Load_1);
             this.pnlVe.ResumeLayout(false);
-            this.pnlVe.PerformLayout();
             this.toolStrip1.ResumeLayout(false);
             this.toolStrip1.PerformLayout();
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -149,5 +172,7 @@
         private System.Windows.Forms.ToolStrip toolStrip1;
         private System.Windows.Forms.ToolStripButton toolStripButton1;
         private System.Windows.Forms.ToolStripButton toolbtnPrint;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Panel pnlQR;
     }
 }
